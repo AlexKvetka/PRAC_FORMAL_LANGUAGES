3962ce9cfd3d0bb11b088b07018ab93c5ae888d6
arapus <postfales@gmil.com>
2022-12-17 23:52:11 +0300

    all_project 3

--- headers_tests/testsCYK.hpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-
-
-extern void CYKParse(std::vector<std::string> w);
-extern std::vector<std::string> terminals_hnf;
-extern std::vector<std::string>  non_terminals_hnf;
-extern std::unordered_map<std::string, std::vector<std::vector<std::string>>> Rules_hnf;
-// Rules_hnf of the grammar
-
